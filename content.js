741a508de4d8a4f5a9f03ec68f80522ddd221bf8
ianden <landendanyluk@gmail.com>
2017-03-15 14:24:24 -0500

    small tweaks

--- content.js
+++ content.js
@@ -1,26 +1,29 @@
-// Runs automagically on button press
-
 function Student(id, grade) {
     this.id = id;
     this.grade = grade;
 }
 
-document.getElementById("testButton").addEventListener("click", captureGrades);
-
-function captureGrades() {
+function sendGrades() {
     var students = []
     var studentCount = document.getElementById('StudentCount').value;
     var outOf = document.getElementById('OutOf').value;
+
     var i; for (i = 0; i < studentCount; i++) {
         var workingRow = document.getElementsByTagName('tr').item(i);
+
         var workingId = workingRow.getElementsByTagName('td')[0].innerHTML;
-        var regx = /\(([^)]+)\)/;
-        workingId = (regx.exec(workingId) != null) ? workingId = regx.exec(workingId)[1] : "cut off";
-        var workingGrade = Math.round(workingRow.getElementsByTagName('input')[1].value / outOf * 100);
+        // extract student number contained in brackets from innerHTML ^
+        var matches = /\(([^)]+)\)/.exec(workingId);
+        // matches = null when student number is cut off
+        if (matches != null) { workingId = matches[1]; }
+        else { workingId = "cutt off"; }
+
+        var workingGrade = workingRow.getElementsByTagName('input')[1].value;
+        workingGrade = Math.round(workingGrade / outOf * 100);
+
         students.push(new Student(workingId, workingGrade));
     }  
     console.log(students);
-}
+} 
 
-
-    
+document.getElementById("testButton").addEventListener("click", sendGrades); 
