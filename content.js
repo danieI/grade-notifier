461e3aef7cade88bdd30f1697a1efb646241ddd0
ianden <landendanyluk@gmail.com>
2017-04-11 14:17:24 -0500

    fix everything again again

--- content.js
+++ content.js
@@ -1,29 +1,40 @@
-function Student(id, grade) {
-    this.id = id;
-    this.grade = grade;
-}
+// function Student(id, grade) {
+//     this.id = id;
+//     this.grade = grade;
+// }
 
-function sendGrades() {
-    var students = []
-    var studentCount = document.getElementById('StudentCount').value;
-    var outOf = document.getElementById('OutOf').value;
+// function sendGrades() {
+//     var students = []
+//     var studentCount = document.getElementById('StudentCount').value;
+//     var outOf = document.getElementById('OutOf').value;
 
-    var i; for (i = 0; i < studentCount; i++) {
-        var workingRow = document.getElementsByTagName('tr').item(i);
+//     var i; for (i = 0; i < studentCount; i++) {
+//         var workingRow = document.getElementsByTagName('tr').item(i);
 
-        var workingId = workingRow.getElementsByTagName('td')[0].innerHTML;
-        // extract student number contained in brackets from innerHTML ^
-        var matches = /\(([^)]+)\)/.exec(workingId);
-        // matches = null when student number is cut off
-        if (matches != null) { workingId = matches[1]; }
-        else { workingId = "cutt off"; }
+//         var workingId = workingRow.getElementsByTagName('td')[0].innerHTML;
+//         // extract student number contained in brackets from innerHTML ^
+//         var matches = /\(([^)]+)\)/.exec(workingId);
+//         // matches = null when student number is cut off
+//         if (matches != null) { workingId = matches[1]; }
+//         else { workingId = "cutt off"; }
 
-        var workingGrade = workingRow.getElementsByTagName('input')[1].value;
-        workingGrade = Math.round(workingGrade / outOf * 100);
+//         var workingGrade = workingRow.getElementsByTagName('input')[1].value;
+//         workingGrade = Math.round(workingGrade / outOf * 100);
 
-        students.push(new Student(workingId, workingGrade));
-    }  
-    console.log(students);
-} 
+//         students.push(new Student(workingId, workingGrade));
+//     }  
+//     sendGrades();
+//     console.log(students);
+// } 
 
-document.getElementById("testButton").addEventListener("click", sendGrades); 
+// Listen for messages
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    // If the received message has the expected format...
+    if (msg.text === 'report_back') {
+        // Call the specified callback, passing
+        // the web-page's DOM content as argument
+        sendResponse(document.all[0].outerHTML);
+    }
+});
+
+// document.getElementById("testButton").addEventListener("click", sendGrades); 
